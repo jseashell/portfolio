08552a19af3e45a91404bd59a1afe97a26576fa3
John Schellinger <schellingerj1@gmail.com>
2021-08-14 16:37:28 -0400

    Create about page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,9 @@
 import { RouterModule, Routes } from '@angular/router';
 
+import { AboutComponent } from './about/about.component';
 import { NgModule } from '@angular/core';
 
-const routes: Routes = [];
+const routes: Routes = [{ path: 'about', component: AboutComponent }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ src/app/position/position.component.ts
@@ -0,0 +1,26 @@
+import { Component, Input, OnInit } from '@angular/core';
+
+import { Position } from './position.model';
+
+@Component({
+  selector: 'app-position',
+  templateUrl: './position.component.html',
+  styleUrls: ['./position.component.css'],
+})
+export class PositionComponent implements OnInit {
+  @Input() position!: Position;
+  startDate: string;
+  endDate: string;
+
+  constructor() {}
+
+  ngOnInit(): void {
+    this.startDate = new Date(this.position.startDate).toLocaleDateString();
+
+    if (this.position.endDate) {
+      this.endDate = new Date(this.position.endDate).toLocaleDateString();
+    } else {
+      this.endDate = 'Present';
+    }
+  }
+}

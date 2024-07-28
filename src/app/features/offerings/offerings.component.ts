b71039b7e8ff104847fb9fe8fff98c9e30192425
John Schellinger <35895180+jseashell@users.noreply.github.com>
2024-07-28 16:18:27 +0000

    tabs on mobile for offerings section

--- src/app/features/offerings/offerings.component.ts
+++ src/app/features/offerings/offerings.component.ts
@@ -1,8 +1,10 @@
 import { AsyncPipe } from '@angular/common';
+import { BreakpointObserver } from '@angular/cdk/layout';
 import { Component, inject, OnInit } from '@angular/core';
 import { MatCardModule } from '@angular/material/card';
 import { MatGridListModule } from '@angular/material/grid-list';
-import { Observable } from 'rxjs';
+import { MatTabsModule } from '@angular/material/tabs'
+import { Observable, map} from 'rxjs';
 import { OfferingsCardComponent } from './offerings-card/offerings-card.component';
 import { OfferingsCard } from './offerings.interface';
 import { OfferingsService } from './offerings.service';
@@ -10,16 +12,20 @@ import { OfferingsService } from './offerings.service';
 @Component({
   selector: 'app-offerings',
   standalone: true,
-  imports: [AsyncPipe, MatCardModule, MatGridListModule, OfferingsCardComponent],
-  providers: [OfferingsService],
+  imports: [AsyncPipe, MatCardModule, MatGridListModule, MatTabsModule, OfferingsCardComponent],
+  providers: [BreakpointObserver, OfferingsService],
   templateUrl: './offerings.component.html',
   styleUrl: './offerings.component.css',
 })
 export class OfferingsComponent implements OnInit {
   private offeringsService = inject(OfferingsService);
   cards$!: Observable<OfferingsCard[]>;
+  isMobile$!: Observable<boolean>;
+  
+  constructor(private bo: BreakpointObserver) {}
 
   ngOnInit(): void {
     this.cards$ = this.offeringsService.cards$;
+    this.isMobile$ = this.bo.observe('(max-width: 1020px)').pipe(map(state => state.matches))
   }
 }

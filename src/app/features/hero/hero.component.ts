b1c69cdb0fb41a7ece452e1dcb4fa82bb956e0f9
John Schellinger <schellingerj1@gmail.com>
2024-08-02 18:29:33 -0400

    Mobile hero layout

--- src/app/features/hero/hero.component.ts
+++ src/app/features/hero/hero.component.ts
@@ -1,15 +1,16 @@
 import { animate, style, transition, trigger } from '@angular/animations';
-import { AsyncPipe } from '@angular/common';
+import { BreakpointObserver } from '@angular/cdk/layout';
+import { AsyncPipe, NgClass } from '@angular/common';
 import { Component, inject, OnInit } from '@angular/core';
 import { MatCardModule } from '@angular/material/card';
-import { BehaviorSubject, switchMap, tap, timer } from 'rxjs';
+import { BehaviorSubject, map, Observable, switchMap, tap, timer } from 'rxjs';
 import { HeroService } from './hero.service';
 
 @Component({
   selector: 'app-hero',
   standalone: true,
-  imports: [AsyncPipe, MatCardModule],
-  providers: [HeroService],
+  imports: [AsyncPipe, MatCardModule, NgClass],
+  providers: [BreakpointObserver, HeroService],
   templateUrl: './hero.component.html',
   styleUrl: './hero.component.scss',
   animations: [
@@ -40,7 +41,12 @@ export class HeroComponent implements OnInit {
     'Code Craftsman',
   ];
 
+  private bo = inject(BreakpointObserver);
+  isMobile$!: Observable<boolean>;
+
   ngOnInit(): void {
+    this.isMobile$ = this.bo.observe('(max-width: 768px)').pipe(map((state) => state.matches));
+
     const defaultCssDuration = 800;
     timer(2 * defaultCssDuration)
       .pipe(

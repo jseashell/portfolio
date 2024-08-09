216c54c0b95a0d01051f9b50ae107b9e6e14368d
John Schellinger <schellingerj1@gmail.com>
2024-08-09 07:42:27 -0400

    Import inter font with jsdelivr cdn

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,4 @@ import { bootstrapApplication } from '@angular/platform-browser';
 import { AppComponent } from './app/app.component';
 import { appConfig } from './app/app.config';
 
-import '@fontsource/inter';
-
 bootstrapApplication(AppComponent, appConfig).catch((err) => console.error(err));

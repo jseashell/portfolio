d1b469f9332fe8d522c582b05d961d2b39baf4ad
John Schellinger <schellingerj1@gmail.com>
2024-10-25 21:49:17 -0400

    Updates to projects section

---
+++ src/app/features/home/projects/project.service.ts
@@ -0,0 +1,88 @@
+import { Observable, of } from 'rxjs';
+
+export interface Project {
+  label: string;
+  excerpt: string;
+  thumbnail: string;
+  alt: string;
+  buttons: Array<{
+    href: string;
+    cta: string;
+    matIcon?: string;
+    svgIcon?: string;
+  }>;
+}
+
+export class ProjectService {
+  get projects$(): Observable<Project[]> {
+    return of([
+      {
+        label: 'KDot Lyrics',
+        excerpt:
+          'Random Kendrick Lamar lyrics at the click of a button. I built a Golang program to seed an AWS DynamoDB table with lyrics. The web app is written with Angular. The data is fetched through AWS Lambda.',
+        thumbnail: '/images/kdot-lyrics.png',
+        alt: 'Screenshot',
+        buttons: [
+          {
+            href: 'https://kdotlyrics.com',
+            cta: 'Visit',
+            matIcon: 'open_in_new',
+          },
+          {
+            href: 'https://github.com/jseashell/kdot-lyrics-client',
+            cta: 'Web App',
+            svgIcon: 'github',
+          },
+          {
+            href: 'https://github.com/jseashell/kdot-lyrics-api',
+            cta: 'API',
+            svgIcon: 'github',
+          },
+          {
+            href: 'https://github.com/jseashell/lyrics-db-seeder',
+            cta: 'Scraper',
+            svgIcon: 'github',
+          },
+        ],
+      },
+      {
+        label: 'EZMC CLI',
+        excerpt:
+          'A server management CLI for self-hosting Minecraft Java Edition in AWS Elastic Container Service. The server is configured with a free, open-source Cloudformation template.',
+        thumbnail: '/images/ezmc.png',
+        alt: 'Screenshot',
+        buttons: [
+          {
+            href: 'https://jseashell.github.io/ezmc-cli',
+            cta: 'Visit',
+            matIcon: 'open_in_new',
+          },
+          {
+            href: 'https://github.com/jseashell/ezmc-cli',
+            cta: 'Github',
+            svgIcon: 'github',
+          },
+        ],
+      },
+      {
+        label: 'APM Pulse',
+        excerpt:
+          'Game-style APM counter in your VS Code bottom status bar that track your actions per minute while coding. It features command palette and settings integrations.',
+        thumbnail: '/images/apm-pulse.gif',
+        alt: 'Demo gif',
+        buttons: [
+          {
+            href: 'https://marketplace.visualstudio.com/items?itemName=jseashell.apm-pulse',
+            cta: 'Marketplace',
+            svgIcon: 'vscode',
+          },
+          {
+            href: 'https://github.com/jseashell/apm-pulse',
+            cta: 'Github',
+            svgIcon: 'github',
+          },
+        ],
+      },
+    ]);
+  }
+}
--- src/app/features/home/projects/projects.component.ts
+++ src/app/features/home/projects/projects.component.ts
@@ -1,13 +1,40 @@
-import { Component } from '@angular/core';
+import { AsyncPipe } from '@angular/common';
+import { AfterViewInit, Component, inject, ViewChild, ViewChildren } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatChipsModule } from '@angular/material/chips';
 import { MatIconModule } from '@angular/material/icon';
-import { MatTabsModule } from '@angular/material/tabs';
+import { MatTab, MatTabGroup, MatTabsModule } from '@angular/material/tabs';
+import { ProjectService } from './project.service';
 @Component({
   selector: 'app-projects',
   standalone: true,
-  imports: [MatButtonModule, MatChipsModule, MatIconModule, MatTabsModule],
+  imports: [AsyncPipe, MatButtonModule, MatChipsModule, MatIconModule, MatTabsModule],
+  providers: [ProjectService],
   templateUrl: './projects.component.html',
   styleUrl: './projects.component.css',
 })
-export class PortfolioComponent {}
+export class PortfolioComponent implements AfterViewInit {
+  service = inject(ProjectService);
+  @ViewChild(MatTabGroup) group!: MatTabGroup;
+  @ViewChildren(MatTab) tabs!: MatTab[];
+
+  tabNumber!: number;
+  selected = 0;
+  SWIPE_ACTION = { LEFT: 'swipeleft', RIGHT: 'swiperight' };
+
+  ngAfterViewInit() {
+    this.tabNumber = this.tabs.length;
+  }
+
+  swipe(eType: string) {
+    console.log(eType);
+    if (eType === this.SWIPE_ACTION.LEFT && this.selected > 0) {
+      console.log('movin left');
+      this.selected--;
+    } else if (eType === this.SWIPE_ACTION.RIGHT && this.selected + 1 < this.tabNumber) {
+      console.log('movin right');
+      this.selected++;
+    }
+    console.log(this.selected);
+  }
+}

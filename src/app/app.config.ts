10cb3452d81f0b60a826bb4fde829985a2ad4b58
John Schellinger <john.schellinger@genesys.com>
2026-02-07 17:52:16 -0500

    Initial commit

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,20 +1,11 @@
-import { provideHttpClient } from '@angular/common/http';
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
-import { provideAnimations } from '@angular/platform-browser/animations';
-import { provideRouter, withInMemoryScrolling } from '@angular/router';
+import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
+import { provideRouter } from '@angular/router';
+
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    provideHttpClient(),
-    provideAnimations(),
-    provideZoneChangeDetection({ eventCoalescing: true }),
-    provideRouter(
-      routes,
-      withInMemoryScrolling({
-        anchorScrolling: 'enabled',
-        scrollPositionRestoration: 'enabled',
-      }),
-    ),
-  ],
+    provideBrowserGlobalErrorListeners(),
+    provideRouter(routes)
+  ]
 };
